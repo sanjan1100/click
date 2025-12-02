2f0074156447d2596ab18040a00f5549e6a16e8b
sanjan <sanjanpoojary36@gmail.com>
2025-12-02 16:02:32 +0530

    commit

---
+++ server.js
@@ -0,0 +1,13 @@
+
+const express = require("express");
+const cors = require("cors"); const app = express();
+
+app.use(cors());
+
+app.get("/hello", (req, res) => {
+    res.send("Hello");
+});
+
+app.listen(3000, () => {
+    console.log("Server running on http://localhost:3000");
+});
